8b0967cdcbb29056bb5f4bf1bc5d1cd6c019c6f1
hzheng-ledger <71653044+hzheng-ledger@users.noreply.github.com>
2022-08-03 10:29:31 +0200

    move tron related code to tron folder

--- libs/ledger-live-common/src/account/serialization.ts
+++ libs/ledger-live-common/src/account/serialization.ts
@@ -1,10 +1,8 @@
 import { BigNumber } from "bignumber.js";
-import type {
-  TronAccount,
-  TronAccountRaw,
-  TronResources,
-  TronResourcesRaw,
-} from "../families/tron/types";
+import {
+  toTronResourcesRaw,
+  fromTronResourcesRaw,
+} from "../families/tron/serialization";
 import {
   toBitcoinResourcesRaw,
   fromBitcoinResourcesRaw,
@@ -85,6 +83,8 @@ import {
 } from "../families/crypto_org/types";
 import { SolanaAccount, SolanaAccountRaw } from "../families/solana/types";
 import { TezosAccount, TezosAccountRaw } from "../families/tezos/types";
+import type { TronAccount, TronAccountRaw } from "../families/tron/types";
+
 import { getAccountBridge } from "../bridge";
 
 export { toCosmosResourcesRaw, fromCosmosResourcesRaw };
@@ -95,6 +95,7 @@ export { toElrondResourcesRaw, fromElrondResourcesRaw };
 export { toCryptoOrgResourcesRaw, fromCryptoOrgResourcesRaw };
 export { toCardanoResourceRaw, fromCardanoResourceRaw };
 export { toSolanaResourcesRaw, fromSolanaResourcesRaw };
+export { toTronResourcesRaw, fromTronResourcesRaw };
 
 export function toBalanceHistoryRaw(b: BalanceHistory): BalanceHistoryRaw {
   return b.map(({ date, value }) => [date.toISOString(), value.toString()]);
@@ -303,153 +304,7 @@ export const fromOperationRaw = (
 
   return res;
 };
-export const toTronResourcesRaw = ({
-  frozen,
-  delegatedFrozen,
-  votes,
-  tronPower,
-  energy,
-  bandwidth,
-  unwithdrawnReward,
-  lastWithdrawnRewardDate,
-  lastVotedDate,
-  cacheTransactionInfoById: cacheTx,
-}: TronResources): TronResourcesRaw => {
-  const frozenBandwidth = frozen.bandwidth;
-  const frozenEnergy = frozen.energy;
-  const delegatedFrozenBandwidth = delegatedFrozen.bandwidth;
-  const delegatedFrozenEnergy = delegatedFrozen.energy;
-  const cacheTransactionInfoById = {};
 
-  for (const k in cacheTx) {
-    const { fee, blockNumber, withdraw_amount, unfreeze_amount } = cacheTx[k];
-    cacheTransactionInfoById[k] = [
-      fee,
-      blockNumber,
-      withdraw_amount,
-      unfreeze_amount,
-    ];
-  }
-
-  return {
-    frozen: {
-      bandwidth: frozenBandwidth
-        ? {
-            amount: frozenBandwidth.amount.toString(),
-            expiredAt: frozenBandwidth.expiredAt.toISOString(),
-          }
-        : undefined,
-      energy: frozenEnergy
-        ? {
-            amount: frozenEnergy.amount.toString(),
-            expiredAt: frozenEnergy.expiredAt.toISOString(),
-          }
-        : undefined,
-    },
-    delegatedFrozen: {
-      bandwidth: delegatedFrozenBandwidth
-        ? {
-            amount: delegatedFrozenBandwidth.amount.toString(),
-          }
-        : undefined,
-      energy: delegatedFrozenEnergy
-        ? {
-            amount: delegatedFrozenEnergy.amount.toString(),
-          }
-        : undefined,
-    },
-    votes,
-    tronPower,
-    energy: energy.toString(),
-    bandwidth: {
-      freeUsed: bandwidth.freeUsed.toString(),
-      freeLimit: bandwidth.freeLimit.toString(),
-      gainedUsed: bandwidth.gainedUsed.toString(),
-      gainedLimit: bandwidth.gainedLimit.toString(),
-    },
-    unwithdrawnReward: unwithdrawnReward.toString(),
-    lastWithdrawnRewardDate: lastWithdrawnRewardDate
-      ? lastWithdrawnRewardDate.toISOString()
-      : undefined,
-    lastVotedDate: lastVotedDate ? lastVotedDate.toISOString() : undefined,
-    cacheTransactionInfoById,
-  };
-};
-export const fromTronResourcesRaw = ({
-  frozen,
-  delegatedFrozen,
-  votes,
-  tronPower,
-  energy,
-  bandwidth,
-  unwithdrawnReward,
-  lastWithdrawnRewardDate,
-  lastVotedDate,
-  cacheTransactionInfoById: cacheTransactionInfoByIdRaw,
-}: TronResourcesRaw): TronResources => {
-  const frozenBandwidth = frozen.bandwidth;
-  const frozenEnergy = frozen.energy;
-  const delegatedFrozenBandwidth = delegatedFrozen.bandwidth;
-  const delegatedFrozenEnergy = delegatedFrozen.energy;
-  const cacheTransactionInfoById = {};
-
-  if (cacheTransactionInfoByIdRaw) {
-    for (const k in cacheTransactionInfoByIdRaw) {
-      const [fee, blockNumber, withdraw_amount, unfreeze_amount] =
-        cacheTransactionInfoByIdRaw[k];
-      cacheTransactionInfoById[k] = {
-        fee,
-        blockNumber,
-        withdraw_amount,
-        unfreeze_amount,
-      };
-    }
-  }
-
-  return {
-    frozen: {
-      bandwidth: frozenBandwidth
-        ? {
-            amount: new BigNumber(frozenBandwidth.amount),
-            expiredAt: new Date(frozenBandwidth.expiredAt),
-          }
-        : undefined,
-      energy: frozenEnergy
-        ? {
-            amount: new BigNumber(frozenEnergy.amount),
-            expiredAt: new Date(frozenEnergy.expiredAt),
-          }
-        : undefined,
-    },
-    delegatedFrozen: {
-      bandwidth: delegatedFrozenBandwidth
-        ? {
-            amount: new BigNumber(delegatedFrozenBandwidth.amount),
-          }
-        : undefined,
-      energy: delegatedFrozenEnergy
-        ? {
-            amount: new BigNumber(delegatedFrozenEnergy.amount),
-          }
-        : undefined,
-    },
-    votes,
-    tronPower,
-    energy: new BigNumber(energy),
-    bandwidth: {
-      freeUsed: new BigNumber(bandwidth.freeUsed),
-      freeLimit: new BigNumber(bandwidth.freeLimit),
-      gainedUsed: new BigNumber(bandwidth.gainedUsed),
-      gainedLimit: new BigNumber(bandwidth.gainedLimit),
-    },
-    unwithdrawnReward: new BigNumber(unwithdrawnReward),
-    lastWithdrawnRewardDate: lastWithdrawnRewardDate
-      ? new Date(lastWithdrawnRewardDate)
-      : undefined,
-    lastVotedDate: lastVotedDate ? new Date(lastVotedDate) : undefined,
-    cacheTransactionInfoById,
-  };
-};
 export function fromSwapOperationRaw(raw: SwapOperationRaw): SwapOperation {
   const { fromAmount, toAmount } = raw;
   return {
